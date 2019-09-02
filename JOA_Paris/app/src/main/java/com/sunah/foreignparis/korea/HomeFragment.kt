1c164eac53b29bbf394fd6e19bc40b5dc058a15e
Sunah <kosah302@gmail.com>
2019-09-02 16:53:34 +0200

    add HomeFragment

--- JOA_Paris/app/src/main/java/com/sunah/foreignparis/korea/HomeFragment.kt
+++ JOA_Paris/app/src/main/java/com/sunah/foreignparis/korea/HomeFragment.kt
@@ -7,20 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.sunah.foreignparis.korea.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
+/*
+data class CategoriesNames(
+    var nameOne: String = "",
+    var nameTwo: String = "",
+    var nameThree: String = "",
+    var nameFour: String = "")
+*/
+
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
-        }
+        savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
+        return binding.root
     }
-
-
 }
--- JOA_Paris/app/src/main/java/com/sunah/foreignparis/korea/MainActivity.kt
+++ JOA_Paris/app/src/main/java/com/sunah/foreignparis/korea/MainActivity.kt
@@ -2,12 +2,15 @@ package com.sunah.foreignparis.korea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.sunah.foreignparis.korea.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding =  DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
