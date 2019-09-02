25abd644a14fa1ebb57730e076929da62a401e71
Sunah <kosah302@gmail.com>
2019-09-02 11:05:53 +0200

    init Firebase Firestore

--- JOA_Paris/app/src/main/java/com/sunah/foreignparis/korea/MainActivity.kt
+++ JOA_Paris/app/src/main/java/com/sunah/foreignparis/korea/MainActivity.kt
@@ -2,11 +2,14 @@ package com.sunah.foreignparis.korea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Locale.getDefault()
     }
 }
